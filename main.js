8961f30f8f7ee61c435aa461322f5b8207bb82cd
dario-ajdadi <dario.ajdadi@xing.com>
2025-10-05 19:52:36 +0200

    initiliazing rails server on electron

--- main.js
+++ main.js
@@ -6,9 +6,38 @@ const createWindow = () => {
     height: 600,
   });
 
-  win.loadFile("index.html");
+  // win.loadFile("index.html");
+  // and load the index.html of the app.
+  win.loadURL("http://localhost:3000");
+
+  // Emitted when the window is closed.
+  win.on("closed", function () {
+    railsApp.kill("SIGINT");
+  });
 };
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow();
+  const railsApp = require('child_process').spawn('rails', ['s'])
+
+  console.log("starting rails server... waiting 5 seconds")
+
+  async function start() {
+    const res = await fetch('http://localhost:3000')
+    console.log('rails server started')
+    createWindow(railsApp)
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow(railsApp)
+    })
+  }
+
+  setTimeout(() => start(), 5000)
+})
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
